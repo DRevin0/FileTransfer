68cc39a369f89a1353e054fa4b31122a50a17db5
DRevin0 <dimareva2000@gmail.com>
2026-09-15 16:22:04 +0300

    Add infinite loop for the server, Ctrl+C(SIGINT) handling, add .gitignore

--- udpClient.c
+++ udpClient.c
@@ -5,8 +5,8 @@ int main(int argc, char *argv[]){
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE] = "Watermellon for everyone!";
 
-    if(argc != 3){
-        printf("%s<port-number><ip-addr>\n", argv[0]);
+    if(argc != 4){
+        printf("%s<port-number><ip-addr><file-to-send>\n", argv[0]);
         return -1;
     }
     client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -18,13 +18,16 @@ int main(int argc, char *argv[]){
     validate_convert_port(argv[1], &server_addr);
     validate_convert_addr(argv[2], &server_addr);
 
+    char *filename = argv[3];
+    snprintf(buffer, BUFFER_SIZE, "%s", filename);
+    // TODO:
     ret = sendto(client_socket, buffer, strlen(buffer), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if(ret<0){
         perror("sendto");
         (void)close(client_socket);
         return -3;
     }
-    printf("Messagesent: %s\n", buffer);
+    printf("Инициирована отправка файла: %s\n", buffer);
     (void)close(client_socket);
     return 0;
 }
--- udpServer.c
+++ udpServer.c
@@ -1,7 +1,14 @@
 #include "utils.h"
+#include <signal.h>
 // argc - количество аргументов при запуске, argv - массив аргументов. Пр: ./server 8080 - argc == 2; argv[0] = ./server;argv[1]=8080
+int server_socket;
+void handle_sigint(int sig) {
+    printf("\nПолучен сигнал завершения. Освобождаю порт и выключаю сервер...\n");
+    close(server_socket);
+    exit(0); // Штатно завершаем программу
+}
+
 int main(int argc, char *argv[]){
-    int server_socket;//дескриптор
     int ret;// хранение ответов системных функций
     struct sockaddr_in server_addr, client_addr;//создаем готовые структуры(определены в системных библиотеках)
     socklen_t addr_size = sizeof(client_addr);//размер структуры адреса
@@ -18,6 +25,8 @@ int main(int argc, char *argv[]){
         perror("Socketfailed");
         return -2;
     }
+    
+    signal(SIGINT, handle_sigint);
 
     server_addr.sin_family = AF_INET;// подтверждаем ipv4
     server_addr.sin_addr.s_addr = INADDR_ANY; // слушаем запросы со всех сетевых карт
@@ -30,15 +39,16 @@ int main(int argc, char *argv[]){
         (void)close(server_socket);
         return -3;
     }
-
-    //аргументы: сокет;куда записывать данные;сколько байт данных можно записать;флаги = 0;адрес структуры клиента(ip и порт того кто пишет);и размер структуры.
-    ret = recvfrom(server_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &addr_size);
-    if(ret < 0){
-        perror("recvfrom");
-        (void)close(server_socket);
-        return -4;
+    printf("Сервер запущен на порту: %s, и ожидает запросы...\n", argv[1]);
+    while(1){
+        ret = recvfrom(server_socket, buffer, BUFFER_SIZE-1, 0, (struct sockaddr *)&client_addr, &addr_size);
+        if(ret<0){
+            perror("recvfrom error");
+            continue;
+        }
+        buffer[ret] = '\0';
+        printf("Получены данные от клиента: %s\n", buffer);
     }
-    printf("reveived: %s\n", buffer);
     (void)close(server_socket);
     return 0;
 }
