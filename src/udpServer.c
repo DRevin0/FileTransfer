b70225a14fee446fb86de9df4d833325bb7edd77
DRevin0 <dimareva2000@gmail.com>
2026-09-20 16:18:24 +0300

    Added a check for the number of packets; removed a vulnerability that allowed escaping from the client to the server.

--- src/udpServer.c
+++ src/udpServer.c
@@ -5,24 +5,32 @@
 #include <sys/socket.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-// argc - количество аргументов при запуске, argv - массив аргументов. Пр: ./server 8080 - argc == 2; argv[0] = ./server;argv[1]=8080
+#include <libgen.h>
 int server_socket;
 void handle_sigint(int sig) {
     printf("\nПолучен сигнал завершения. Освобождаю порт и выключаю сервер...\n");
     close(server_socket);
     exit(0); // Штатно завершаем программу
 }
-void receive_file(int server_socket, FILE *file, struct sockaddr_in *client_addr, socklen_t *addr_size){
+int receive_file(int server_socket, FILE *file, struct sockaddr_in *client_addr, socklen_t *addr_size){
     Pack packet;
+    int count = 0;
     while(1){
+        *addr_size = sizeof(*client_addr);
         int ret = recvfrom(server_socket, &packet, sizeof(Pack), 0, (struct sockaddr *)client_addr, addr_size);
         if(ret<0){
             perror("recvfrom packet error");
-            break;
+            return -1;
         }
         if(packet.data_size == 0){
             printf("Получен маркер окончания файла\n");
-            break;
+            if(count != packet.current_number){
+                printf("Файл получен не полностью. (получено %d из %d)\n", count, packet.current_number);
+                return -1;
+            }
+            else{
+                return 0;
+            }
         }
         long offset = (long)packet.current_number*BUFFER_SIZE;
         if(fseek(file, offset, SEEK_SET)!= 0){
@@ -30,6 +38,7 @@ void receive_file(int server_socket, FILE *file, struct sockaddr_in *client_addr
             continue;
         }
         fwrite(packet.data, 1, packet.data_size, file);
+        count += 1;
         printf("Записан пакет %d (смещение: %ld, байт: %zu\n)\n", packet.current_number, offset, packet.data_size);
     }
 }
@@ -61,17 +70,16 @@ void hash_file_mmap(int fd, char *out_hash){
 }
 
 int main(int argc, char *argv[]){
-    int ret;// хранение ответов системных функций
-    struct sockaddr_in server_addr, client_addr;//создаем готовые структуры(определены в системных библиотеках)
-    socklen_t addr_size = sizeof(client_addr);//размер структуры адреса
-    char buffer[BUFFER_SIZE];//коробка для приема сообщений
+    int ret;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
 
     if (argc != 2){
         printf("%s <port-number>", argv[0]);
         return -1;
     }
 
-    //Создаем сокет. af_inet - ipv4, остальное для udp
     server_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if(server_socket < 0){
         perror("Socketfailed");
@@ -80,11 +88,10 @@ int main(int argc, char *argv[]){
     
     signal(SIGINT, handle_sigint);
 
-    server_addr.sin_family = AF_INET;// подтверждаем ipv4
-    server_addr.sin_addr.s_addr = INADDR_ANY; // слушаем запросы со всех сетевых карт
-    validate_convert_port(argv[1], &server_addr);//Наша функция
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    validate_convert_port(argv[1], &server_addr);
 
-    // связываем конкретные ip-адрес и порт с дескриптором, приводим типы для ipv4 вторым арг.
     ret = bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if(ret < 0){
         perror("Bind failed");
@@ -94,14 +101,19 @@ int main(int argc, char *argv[]){
     printf("Сервер запущен на порту: %s, и ожидает запросы...\n", argv[1]);
 
     while(1){
+        addr_size = sizeof(client_addr);
         ret = recvfrom(server_socket, buffer, BUFFER_SIZE-1, 0, (struct sockaddr *)&client_addr, &addr_size);
         if(ret<0){
             perror("recvfrom error");
             continue;
         }
         buffer[ret] = '\0';
-        char output_filename[BUFFER_SIZE+10];
-        snprintf(output_filename, sizeof(output_filename), "recv_%s", buffer);
+        char *clean_filename = basename(buffer);
+        if(strcmp(clean_filename, ".")==0 || strcmp(clean_filename, "..")==0 || strlen(clean_filename) == 0){
+            clean_filename = "unnamed_file";
+        }
+        char output_filename[BUFFER_SIZE+16];
+        snprintf(output_filename, sizeof(output_filename), "recv_%s", clean_filename);
         printf("\nИнициирован прием файла: %s (сохраняем как %s)", buffer, output_filename);
 
         FILE *file = fopen(output_filename, "wb+");
@@ -110,7 +122,16 @@ int main(int argc, char *argv[]){
             continue;
         }
         
-        receive_file(server_socket, file, &client_addr, &addr_size);
+        if(receive_file(server_socket, file, &client_addr, &addr_size) < 0){
+            printf("[Gebrochen] Прием прерван: пакеты утеряны. Файл удален.\n");
+            fclose(file);
+            remove(output_filename);
+            char dummy[SHA256_HEX_SIZE];
+            addr_size = sizeof(client_addr);
+            recvfrom(server_socket, dummy, sizeof(dummy) - 1, MSG_DONTWAIT, (struct sockaddr *)&client_addr, &addr_size);
+
+            continue;
+        }
         fflush(file);
         char local_hash[SHA256_HEX_SIZE];
         int fd = fileno(file);
@@ -118,6 +139,7 @@ int main(int argc, char *argv[]){
         fclose(file);
 
         char remote_hash[SHA256_HEX_SIZE];
+        addr_size = sizeof(client_addr);
         ret = recvfrom(server_socket, remote_hash, sizeof(remote_hash)-1, 0, (struct sockaddr *)&client_addr,&addr_size);
         if(ret<0){
             perror("recvfrom hash error");
@@ -131,7 +153,8 @@ int main(int argc, char *argv[]){
             printf("[Alles ist gut] Файл доставлен без потерь и поверждений\n");
         }
         else{
-            printf("[Gebrochen] Несовпадение хэшей! Пакеты были утеряны или повреждены!");
+            printf("[Gebrochen] Несовпадение хэшей! Пакеты были утеряны или повреждены! Файл не сохранен");
+            remove(output_filename);
         }
     }
     (void)close(server_socket);
